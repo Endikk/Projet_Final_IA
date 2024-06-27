fed0f7d98ebce239ff245216c2739b03a05cae80
Endikk <103244724+Endikk@users.noreply.github.com>
2024-06-27 22:32:31 +0200

    new for livrable 2
    new for deliverable 2

--- main.py
+++ main.py
@@ -1,132 +1,135 @@
-# Importation des modules système
 import os
-import sys
+import json
+import requests
 from time import sleep
 from datetime import datetime
-import json
 from os.path import join, realpath, dirname, exists
-
-# Importation des modules tiers
-from dotenv import load_dotenv
 from PIL import Image
 import imagehash
-import requests
+import numpy as np
+import cv2
+import torch
+import sys
 
-# Variables d'environnement qui prendront les valeurs du fichier .env à la racine du projet
-load_dotenv()
-TOKEN = os.getenv("TOKEN")
-IMAGE_URL = os.getenv("IMAGE_URL")
-BASE_URL = "https://app.heartex.com/storage-data/uploaded/?filepath="
+# Ajouter le chemin 'src' à sys.path
+sys.path.append(join(dirname(realpath(__file__)), 'src'))
 
-# Paths
+from model import LargeModel  # Importer le modèle choisi (LargeModel)
+
+# Chemins
 PROJECT_PATH = dirname(realpath(__file__))
 IMAGES_DIR = join(PROJECT_PATH, 'images')
-IMAGES_LABELS_DIR = join(PROJECT_PATH, 'images_labels')
+RESULTS_FILE = join(PROJECT_PATH, 'vehicle_results.json')
+MASK_PATH = join(PROJECT_PATH, 'src','models','ref_image.png')  # Chemin du fichier masque
 
-# Fonctions utilitaires pour les téléchargements d'images
+# Charger les variables d'environnement
+IMAGE_URL = os.getenv("IMAGE_URL")
+TOKEN = os.getenv("TOKEN")
+
+# Fonction pour télécharger une image
 def download_image(url, destination, headers=None):
     try:
         response = requests.get(url, headers=headers, timeout=10)
-        if response.status_code == 200: # Vérifie si la requête a abouti
+        if response.status_code == 200:
             with open(destination, 'wb') as f:
                 f.write(response.content)
-            print(f"\nL'image {os.path.basename(destination)} a été téléchargée avec succès.")
+            print(f"L'image {os.path.basename(destination)} a été téléchargée avec succès.")
             return True
         else:
-            print(url)
             print(f"Échec du téléchargement de l'image {destination}. {response.status_code} {response.reason}")
     except requests.RequestException as e:
         print(f"Erreur lors du téléchargement de l'image {destination}. Détails: {e}")
     return False
 
-# Fonctions utilitaires pour la comparaison d'images
+# Fonction pour vérifier si deux images sont identiques
 def is_identical(image1, image2):
     try:
-        hash1 = imagehash.average_hash(Image.open(image1)) # Calcul du hash de l'image 1 (image précédente)
-        hash2 = imagehash.average_hash(Image.open(image2)) # Calcul du hash de l'image 2 (image actuelle)
+        hash1 = imagehash.average_hash(Image.open(image1))
+        hash2 = imagehash.average_hash(Image.open(image2))
         if (hash1 - hash2) == 0:
             return True
     except Exception:
         pass
     return False
 
-# Fonctions utilitaires pour la suppression d'images si elles sont identiques
+# Fonction pour supprimer une image
 def remove_image(filename):
-    if exists(filename): # Vérifie si le fichier existe
-        os.remove(filename) 
+    if exists(filename):
+        os.remove(filename)
 
-# Fonction principale pour extraire les images à partir d'une URL
-def extract_image(loop=50, wait=60): # Extrait 50 images par défaut avec un intervalle de 60 secondes
+# Fonction principale pour extraire et traiter les images
+def extract_and_identify_vehicles(loop=50, wait=60):
+    if not exists(IMAGES_DIR):
+        os.makedirs(IMAGES_DIR)
+
     images = sorted([f for f in os.listdir(IMAGES_DIR) if f.endswith('.jpg')])
     last = join(IMAGES_DIR, images[-1]) if images else None
+
+    model = LargeModel()  # Initialiser le modèle choisi
+
+    # Charger le masque
+    mask = cv2.imread(MASK_PATH, cv2.IMREAD_GRAYSCALE)
+    if mask is None:
+        raise FileNotFoundError(f"Le masque {MASK_PATH} n'a pas été trouvé.")
+
+    vehicle_results = []
+
     for i in range(loop):
-        if i > 0: 
-            try:
-                sleep(wait) 
-            except KeyboardInterrupt:
-                break
-        # Permet de renommer les images avec un timestamp en format YmdHMS
+        if i > 0:
+            sleep(wait)
+
         filename = join(IMAGES_DIR, f"{datetime.now().strftime('%Y%m%d%H%M%S')}.jpg")
-        download_image(IMAGE_URL, filename)
-        if is_identical(last, filename):
-            print(f'Image {os.path.basename(filename)} identique à la précédente. Suppression...')
-            remove_image(filename)
-        last = filename # Affectation de la dernière image téléchargée
+        if download_image(IMAGE_URL, filename):
+            if is_identical(last, filename):
+                print(f'Image {os.path.basename(filename)} identique à la précédente. Suppression...')
+                remove_image(filename)
+                continue
 
-# Fonction principale pour traiter le fichier JSON et télécharger les images
-def process_json(json_file):
-    if not exists(json_file):
-        print("Fichier JSON introuvable.")
-        return
+            last = filename
 
-    with open(json_file, 'r') as file:
-        data = json.load(file)
-    
-    failed_downloads = [] # Liste des images qui n'ont pas été téléchargées
-    for item in data:
-        try:
-            image_url = f"{BASE_URL}{item['data']['image']}" # URL de l'image complète
-            filename = item['file_upload']
-        except:
-            print('Erreur lors de la récupération des données. JSON Malformé.')
-        
-        if not exists(join(IMAGES_LABELS_DIR, filename)):
-            if not download_image(image_url, join(IMAGES_LABELS_DIR, filename), headers={"Authorization": f"Token {TOKEN}"}):
-                failed_downloads.append(filename)
+            # Charger l'image
+            image = cv2.imread(filename)
+            if image is None:
+                print(f"Impossible de charger l'image {filename}")
+                continue
 
-    if failed_downloads:
-        print(f"\nÉchec du téléchargement de certaines images. {len(data)-len(failed_downloads)}/{len(data)}")
-        print(failed_downloads) # Le nom des images qui n'ont pas été téléchargées
-    else:
-        print("\nToutes les images ont été téléchargées avec succès.")
+            # Redimensionner le masque pour qu'il corresponde à la taille de l'image
+            mask_resized = cv2.resize(mask, (image.shape[1], image.shape[0]))
 
-# Fonction principale pour lancer le script
-def main():
-    args = sys.argv[1:]
-    if args:
-        # Cette partie du script permet de télécharger les images à partir d'une URL
-        if args[0] == 'extract':
-            
-            if not exists(IMAGES_DIR):
-                os.makedirs(IMAGES_DIR)
-            
-            try:
-                extract_image(loop=int(args[1]))
-            except:
-                print("Valeur incorrecte pour le nombre de boucles. Utilisation de la valeur par défaut (50 boucles).")
-                extract_image(loop=50, wait=60)
+            # Appliquer le masque sur l'image
+            masked_image = cv2.bitwise_and(image, image, mask=mask_resized)
 
-        # Cette partie du script permet de télécharger les images à partir d'un fichier JSON
-        elif args[0] == 'annotations':
+            # Détecter les véhicules dans l'image
+            preds = model.detect_image(masked_image)
 
-            if not exists(IMAGES_LABELS_DIR):
-                os.makedirs(IMAGES_LABELS_DIR)
+            # Dessiner les bounding boxes sur l'image
+            model.detect_and_draw_on_image(masked_image, filename)
 
-            if len(args) >= 2:
-                process_json(args[1])
+            # Vérifier si des objets ont été détectés
+            if preds[0].boxes.shape[0] == 0:
+                image_class_counts = {"nothing": 0}
             else:
-                print("Veuillez fournir le chemin vers l'export JSON.")    
+                class_counts = torch.bincount(preds[0].boxes.cls.long(), minlength=8)
+                d = {0: 'Bicycle', 1: 'Bus', 2: 'Car', 3: 'Motor scooter', 4: 'Motorbike', 5: 'Pedestrian', 6: 'Scooter', 7: 'Truck'}
+                CUSTOM_CATEGORIES = {0: 5, 1: 0, 2: 2, 3: 4, 5: 1, 7: 7}
+                image_class_counts = {d[CUSTOM_CATEGORIES[index]]: count for index, count in enumerate(class_counts.tolist()) if count > 0}
+
+            result = {
+                "image": os.path.basename(filename),  # Utiliser uniquement le nom de l'image
+                "categories": image_class_counts
+            }
+
+            vehicle_results.append(result)
+
+            # Afficher les résultats immédiatement après traitement de chaque image
+            print(json.dumps(result, indent=4))
+
+            # Écrire les résultats dans le fichier JSON après chaque image traitée
+            with open(RESULTS_FILE, 'w') as f:
+                json.dump(vehicle_results, f, indent=4)
+
+    print(f"Toutes les images ont été traitées et les résultats sont enregistrés dans {RESULTS_FILE}.")
 
 # Point d'entrée du script
 if __name__ == "__main__":
-    main()
+    extract_and_identify_vehicles(loop=50, wait=60)
